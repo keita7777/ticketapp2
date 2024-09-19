fe96e6c61cf72ee637e0afabe60d040931b3285b
github keita7777 <github mine0kei@gmail.com>
2024-09-20 05:37:59 +0900

    ThemeProviderの作成と適用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import MainNav from "@/components/MainNav";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,14 +30,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
-          <div className="max-w-6xl w-full">
-            <MainNav />
-          </div>
-        </nav>
-        <main className="flex flex-col items-center border-b">
-          <div className="max-w-6xl w-full">{children}</div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+            <div className="max-w-6xl w-full">
+              <MainNav />
+            </div>
+          </nav>
+          <main className="flex flex-col items-center border-b">
+            <div className="max-w-6xl w-full">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
