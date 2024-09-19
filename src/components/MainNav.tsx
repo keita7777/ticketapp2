7a24c071bbdc0715d0da072df4c29fe23f3d3fc6
github keita7777 <github mine0kei@gmail.com>
2024-09-20 04:02:22 +0900

    ヘッダーを作成

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const MainNav = () => {
+  return (
+    <div>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+      <Link href="/users">Users</Link>
+    </div>
+  );
+};
+export default MainNav;
