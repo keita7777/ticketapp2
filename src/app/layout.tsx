5c7c479b8951cd3d252f599b825ca47119bc1e1c
github keita7777 <github mine0kei@gmail.com>
2024-09-20 04:31:17 +0900

    layout.tsxのスタイル調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MainNav />
-        {children}
+        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+          <div className="max-w-6xl w-full">
+            <MainNav />
+          </div>
+        </nav>
+        <main className="flex flex-col items-center border-b">
+          <div className="max-w-6xl w-full">{children}</div>
+        </main>
       </body>
     </html>
   );
