44a0ed4d2304744cf5c386b3053d6b0e84d7fc33
github keita7777 <github mine0kei@gmail.com>
2024-09-21 00:38:16 +0900

    フォームの作成

---
+++ src/app/tickets/new/page.tsx
@@ -0,0 +1,12 @@
+import TicketForm from "@/components/TicketForm";
+
+// import dynamic from "next/dynamic";
+
+// const TicketForm = dynamic(() => import("@/components/TicketForm"), {
+//   ssr: false,
+// });
+
+const NewTicket = () => {
+  return <TicketForm />;
+};
+export default NewTicket;
---
+++ src/components/TicketForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { ticketSchema } from "@/validationSchemas/ticket";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+type TicketFormData = z.infer<typeof ticketSchema>;
+
+const TicketForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
+
+  const form = useForm<TicketFormData>({
+    resolver: zodResolver(ticketSchema),
+  });
+
+  async function onSubmit(values: TicketFormData) {
+    console.log(values);
+  }
+
+  return (
+    <div className="rounded-md border w-full p-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ticket Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Ticket Title..." {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Controller
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description" {...field} />
+            )}
+          />
+          <div className="flex w-full space-x-4">
+            <FormField
+              name="status"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Status</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Status..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="OPEN">Open</SelectItem>
+                      <SelectItem value="STARTED">Started</SelectItem>
+                      <SelectItem value="CLOSED">Closed</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="priority"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Priority</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Priority..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="LOW">Low</SelectItem>
+                      <SelectItem value="MEDIUM">Medium</SelectItem>
+                      <SelectItem value="HIGH">High</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+export default TicketForm;
