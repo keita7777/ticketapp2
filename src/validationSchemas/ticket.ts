14cec9cbae1d3b6b30765a6673c3cdc46ed13512
github keita7777 <github mine0kei@gmail.com>
2024-09-20 12:06:27 +0900

    zodでschemaを作成

---
+++ src/validationSchemas/ticket.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const ticketSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required.").max(65535),
+
+  // status と priorityにはデフォルト値が設定される（schema.prisma）
+  // そのためこれらのフィールドが送信されなくてもエラーにならないように
+  // optional()を設定している
+  status: z.string().min(1, "Status").max(10).optional(),
+  priority: z.string().min(1, "Priority").max(10).optional(),
+});
