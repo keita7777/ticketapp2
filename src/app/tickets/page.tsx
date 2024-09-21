4a18b69da1a63fa9304ca8d2f07b764a6e754ef6
github keita7777 <github mine0kei@gmail.com>
2024-09-21 14:34:42 +0900

    フィルターの実装

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,8 +3,11 @@ import prisma from "../../../prisma/db";
 import DataTable from "./DataTable";
 import { buttonVariants } from "@/components/ui/button";
 import Pagenation from "@/components/pagenation";
+import StatusFilter from "@/components/StatusFilter";
+import { Status } from "@prisma/client";
 
 interface SearchParams {
+  status: Status;
   page: string;
 }
 
@@ -14,22 +17,48 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   // searchParamsのpageにアクセスして値を取得する
   // クエリパラメータがない場合は1となる
   const page = parseInt(searchParams.page) || 1;
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {};
+
+  if (status) {
+    where = {
+      status,
+    };
+  } else {
+    where = {
+      NOT: [
+        {
+          status: "CLOSED" as Status,
+        },
+      ],
+    };
+  }
+
   // データの数を取得する
-  const ticketCount = await prisma.ticket.count();
+  const ticketCount = await prisma.ticket.count({ where });
 
   const tickets = await prisma.ticket.findMany({
+    where,
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
 
   return (
     <div>
-      <Link
-        href="/tickets/new"
-        className={buttonVariants({ variant: "default" })}
-      >
-        New Ticket
-      </Link>
+      <div className="flex gap-2">
+        <Link
+          href="/tickets/new"
+          className={buttonVariants({ variant: "default" })}
+        >
+          New Ticket
+        </Link>
+        <StatusFilter />
+      </div>
       <DataTable tickets={tickets} />
       <Pagenation
         itemCount={ticketCount}
