08da51a509bace7d985e40b829be6103da573061
github keita7777 <github mine0kei@gmail.com>
2024-09-21 15:13:31 +0900

    ソート実装

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,11 +4,12 @@ import DataTable from "./DataTable";
 import { buttonVariants } from "@/components/ui/button";
 import Pagenation from "@/components/pagenation";
 import StatusFilter from "@/components/StatusFilter";
-import { Status } from "@prisma/client";
+import { Status, Ticket } from "@prisma/client";
 
-interface SearchParams {
+export interface SearchParams {
   status: Status;
   page: string;
+  orderBy: keyof Ticket;
 }
 
 const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
@@ -17,6 +18,8 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   // searchParamsのpageにアクセスして値を取得する
   // クエリパラメータがない場合は1となる
   const page = parseInt(searchParams.page) || 1;
+
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt";
 
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
@@ -44,6 +47,9 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
 
   const tickets = await prisma.ticket.findMany({
     where,
+    orderBy: {
+      [orderBy]: "desc",
+    },
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
@@ -59,7 +65,7 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
         </Link>
         <StatusFilter />
       </div>
-      <DataTable tickets={tickets} />
+      <DataTable tickets={tickets} searchParams={searchParams} />
       <Pagenation
         itemCount={ticketCount}
         pageSize={pageSize}
